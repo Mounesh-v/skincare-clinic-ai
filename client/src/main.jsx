dd3e6ef920e010b8ca6d11aef2865e061370f686
Imran Farhat <imranfarhat442@gmail.com>
2026-02-08 21:29:18 +0530

    modified:   client/src/main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+if (typeof window !== 'undefined' && window.location.hostname === '127.0.0.1') {
+  const { protocol, port, pathname, search, hash } = window.location
+  const hostPort = port ? `:${port}` : ''
+  const redirectUrl = `${protocol}//localhost${hostPort}${pathname}${search}${hash}`
+  window.location.replace(redirectUrl)
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
