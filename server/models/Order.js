cca595921b2b761273009b2bac513abc97995b69
mounesh <mouneshv696@gmail.com>
2026-03-25 22:35:56 +0530

    Order api is done

--- server/models/Order.js
+++ server/models/Order.js
@@ -68,6 +68,10 @@ const orderSchema = new mongoose.Schema(
       ],
       default: "Pending",
     },
+    vendor: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Vendor",
+    },
 
     timeline: [
       {
--- server/router/orderRoutes.js
+++ server/router/orderRoutes.js
@@ -5,12 +5,16 @@ import {
   getOrderById,
   updateOrderStatus,
   deleteOrder,
+  getMyOrders,
+  getVendorOrders,
 } from "../controllers/orderController.js";
 import verifyToken, { verifyAdmin } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
 router.post("/", verifyToken, createOrder);          // any logged-in user
+router.get("/my", verifyToken, getMyOrders);          //user
+router.get("/vendor", verifyToken, getVendorOrders);   //vendor
 router.get("/", verifyAdmin, getAllOrders);           // admin only
 router.get("/:id", verifyToken, getOrderById);       // owner or admin
 router.put("/:id", verifyAdmin, updateOrderStatus);  // admin only
